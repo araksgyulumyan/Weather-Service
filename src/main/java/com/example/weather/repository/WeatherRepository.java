c0519f4b58b4f273c2f33b9dfac56f743d2e2c7d
Araks Gyulumyan <araks.gyulumyan@gmail.com>
2022-01-12 17:57:05 +0400

    deleting unused imports

--- src/main/java/com/example/weather/repository/WeatherRepository.java
+++ src/main/java/com/example/weather/repository/WeatherRepository.java
@@ -2,7 +2,6 @@ package com.example.weather.repository;
 
 import com.example.weather.entity.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
